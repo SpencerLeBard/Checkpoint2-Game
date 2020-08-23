b5a77d712975dfe2d8621019372587e2f4afd6e0
SpencerLeBard <slebard@gmail.com>
2020-08-23 10:57:25 -0600

    beta-sunday

--- app.js
+++ app.js
@@ -1,39 +1,102 @@
 let damageElem = document.getElementById('total-fish')
+let spearPrice = document.getElementById('spear-price')
+let netPrice = document.getElementById('net-price')
+let gunPrice = document.getElementById('gun-price')
+let nukePrice = document.getElementById('nuke-price')
+let DrawSpearInventory = document.getElementById('spear-inventory')
+let DrawNetInventory = document.getElementById('net-inventory')
+let DrawGunInventory = document.getElementById('gun-inventory')
+let DrawNukeInventory = document.getElementById('nuke-inventory')
+
 
 let clickUpgrades = {
-  Spear: {
+  spear: {
     cost: 10,
     modifier: 2,
     id: 12
   },
-  Net: {
+
+
+  net: {
     cost: 100,
     modifier: 5,
     id: 123
   },
-  MachineGun: {
-    cost: 10000,
+
+
+  machineGun: {
+    cost: 1000,
     modifier: 10,
     id: 1234
   },
 
-};
+}
 
 
 let automaticUpgrades = {
-  rovers: {
-    price: 600,
-    quantity: 0,
-    multiplier: 20
+  nuke: {
+    cost: 10000,
+    modifier: 100,
+    id: 12345
   }
-};
+}
 
 
 let fisher = {
-  fish: 1
+  fish: 10000,
+  spear: 0,
+  net: 0,
+  machineGun: 0,
+  nuke: 0
 
 }
 
+function cast() {
+  fisher.fish += 1
+  fisher.fish += fisher.spear * 2
+  fisher.fish += fisher.net * 5
+  fisher.fish += fisher.machineGun * 10
+  fisher.fish += fisher.nuke * 100
+  drawDamage();
+}
+
+function equipWeapon(name) {
+
+  let item = clickUpgrades[name]
+
+
+  if (fisher.fish >= item.cost) {
+    fisher[name]++
+    fisher.fish -= item.cost
+    item.cost += 10
+    if (item.name == "spear") {
+      //NOTE MODIFIER Here??
+
+    }
+    drawDamage()
+  }
+}
+
+
+function equipAutoWeapon(name) {
+
+  let item = automaticUpgrades[name]
+
+
+  if (fisher.fish >= item.cost) {
+    fisher.fish++
+    fisher.fish -= item.cost
+    //??
+    fisher.spear += 1
+    //??
+
+
+    drawDamage()
+    drawSpearsInventory()
+    drawSpearPrice()
+  }
+
+}
 
 function drawDamage() {
   let template = `${fisher.fish}`
@@ -41,17 +104,81 @@ function drawDamage() {
 }
 drawDamage()
 
-function cast() {
-  fisher.fish += 1
-  drawDamage();
-}
+// function autocast interval 0 , when buy increase interval 
+//xxxxxxxxxxxxxxxx NOTE Drawing
 
-function equipWeapon(id) {
-  console.log(id);
-  let item = shop.find(i => i.id == id)
-  console.log(item);
+// price
 
-  //check if can buy
-  //increase damage and decrease money
-  //
+function drawSpearPrice() {
+  let template = `${clickUpgrades.spear.cost}`
+  spearPrice.innerText = template
+
 }
+drawSpearPrice()
+
+
+
+function drawNetPrice() {
+  let template = `${clickUpgrades.net.cost}`
+  netPrice.innerText = template
+
+
+}
+drawNetPrice()
+
+function drawGunPrice() {
+  let template = `${clickUpgrades.machineGun.cost}`
+  gunPrice.innerText = template
+
+
+}
+drawGunPrice()
+
+function drawNukePrice() {
+  let template = `${automaticUpgrades.nuke.cost}`
+  nukePrice.innerText = template
+
+
+}
+drawNukePrice()
+
+
+
+// inventory
+
+
+
+function drawSpearsInventory() {
+  let template = `${fisher.spear}`
+  DrawSpearInventory.innerText = template
+
+
+}
+drawSpearsInventory()
+
+function drawNetInventory() {
+  let template = `${fisher.net}`
+  DrawNetInventory.innerText = template
+
+
+}
+drawNetInventory()
+
+
+function drawGunInventory() {
+  let template = `${fisher.machineGun}`
+  DrawGunInventory.innerText = template
+
+
+}
+drawGunInventory()
+
+
+function drawNukeInventory() {
+  let template = `${fisher.nuke}`
+  DrawNukeInventory.innerText = template
+
+
+}
+drawNukeInventory()
+
